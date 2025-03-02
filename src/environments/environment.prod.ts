3e13e8f6b4a9934f5a0f2cf72016803e29d7c5a1
devtachomi <devtech.omi@gmail.com>
2025-03-02 23:25:18 +0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://logd-backend.onrender.com'
+    apiUrl: 'https://logd-be.onrender.com'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://logd-be.onrender.com'
+    apiUrl: 'https://localhost:7002'
 }
